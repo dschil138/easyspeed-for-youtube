fd805f17462b0f2f456453c5efd954cf0cb5d266
dschil138 <davidschiller138@gmail.com>
2023-10-16 16:44:44 -0500

    version 2, add speed choices and drag functionality

--- content.js
+++ content.js
@@ -4,14 +4,30 @@ let longPressTimer;
 let longPressFlag = false;
 let lastVideoElement = null;
 let indicator;
-let playbackSpeed = 2; // Default/Start
+let initialX;
+let minPlaybackSpeed;
+let playbackSpeed;
+let maxPlaybackSpeed;
+
 
 function init() {
-  // Fetch setting from chrome storage 
+  // Get any previous settings from chrome storage 
   // hast to run at the start of each init 
+  chrome.storage.sync.get('minSpeed', function(data) {
+    minPlaybackSpeed = parseFloat(data.minSpeed) || 1.5;
+  }
+  );
+
   chrome.storage.sync.get('speed', function(data) {
     playbackSpeed = parseFloat(data.speed) || 2;
   });
+
+
+  chrome.storage.sync.get('maxSpeed', function(data) {
+    maxPlaybackSpeed = parseFloat(data.maxSpeed) || 3;
+  }
+  );
+
 
   video = document.querySelector('video');
   
@@ -19,26 +35,34 @@ function init() {
     lastVideoElement = video;
     
     indicator = document.createElement('div');
+    indicator.style.fontSize = '1.4em';
     indicator.innerText = `${playbackSpeed}x Speed`;
     indicator.style.position = 'absolute';
     indicator.style.top = '40px';
     indicator.style.right = 'calc(50% - 50px)';
-    indicator.style.borderRadius = '10px';
+    indicator.style.borderRadius = '11px';
     indicator.style.backgroundColor = 'rgba(0, 0, 0, 0.3)';
     indicator.style.color = 'white';
     indicator.style.padding = '8px';
+    indicator.style.paddingLeft = '13px';
+    indicator.style.paddingRight = '13px';
     indicator.style.zIndex = '1000';
     indicator.style.display = 'none';
     
     video.parentElement.appendChild(indicator);
 
     video.addEventListener('mousedown', (e) => {
-        originalSpeed = video.playbackRate;
+      initialX = e.clientX;
+
+        originalSpeed = parseFloat(video.playbackRate);
         longPressTimer = setTimeout(() => {
           video.playbackRate = playbackSpeed;
           indicator.innerText = `${playbackSpeed}x Speed`;
           longPressFlag = true;
           indicator.style.display = 'block';
+
+          video.addEventListener('mousemove', handleMouseMove, true);
+
         }, 250);
     }, true);
 
@@ -46,6 +70,7 @@ function init() {
       clearTimeout(longPressTimer);
       video.playbackRate = originalSpeed;
       indicator.style.display = 'none';
+      video.removeEventListener('mousemove', handleMouseMove, true);
     
       if (longPressFlag) {
         longPressFlag = false;
@@ -55,20 +80,42 @@ function init() {
           const spaceBarEvent = new KeyboardEvent('keydown', {
             bubbles: true,
             cancelable: true,
-            keyCode: 32 
+            keyCode: 32
           });
           document.dispatchEvent(spaceBarEvent);
         }, 10); //10 ms
       }
     }, true);
+    
 
     video.addEventListener('pause', (e) => {
       if (longPressFlag) {
         video.play();  
       }
     }, true);
+  } // End of if statement
+
+
+} // End of init function
+
+
+function handleMouseMove(e) {
+  if (!longPressFlag) return;
+
+  const deltaX = e.clientX - initialX;
+
+  if (deltaX > 42) {
+    video.playbackRate = maxPlaybackSpeed
+    indicator.innerText = `${video.playbackRate}x Speed`;
+  } else if (deltaX < -42) {
+    video.playbackRate = minPlaybackSpeed
+    indicator.innerText = `${video.playbackRate}x Speed`;
+  } else {
+    video.playbackRate = playbackSpeed;
+    indicator.innerText = `${playbackSpeed}x Speed`;
   }
 }
+
 
 // Call it every 2 seconds
 setInterval(init, 2000);
--- popup.js
+++ popup.js
@@ -1,23 +1,56 @@
 document.addEventListener('DOMContentLoaded', function() {
-  const buttons = document.querySelectorAll('.speed-button');
+  const leftButtons = document.querySelectorAll('.left-speed-button');
+  const mainButtons = document.querySelectorAll('.speed-button');
+  const rightButtons = document.querySelectorAll('.right-speed-button');
 
-  // Load any previously saved setting
-  chrome.storage.sync.get('speed', function(data) {
+  // Load any previously saved settings
+  chrome.storage.sync.get(['minSpeed', 'speed', 'maxSpeed'], function(data) {
+    const minSpeed = parseFloat(data.minSpeed) || 1.5;
     const speed = parseFloat(data.speed) || 2;
+    const maxSpeed = parseFloat(data.maxSpeed) || 3;
+    document.querySelector(`[min-data-speed="${minSpeed}"]`).classList.add('selected');
     document.querySelector(`[data-speed="${speed}"]`).classList.add('selected');
+    document.querySelector(`[max-data-speed="${maxSpeed}"]`).classList.add('selected');
   });
 
-  buttons.forEach((button) => {
+
+
+  leftButtons.forEach((button) => {
+    button.addEventListener('click', function() {
+      const minSpeed = this.getAttribute('min-data-speed');
+
+      // Remove 'selected' class from the buttons but add it to just to clicked button
+      leftButtons.forEach((btn) => btn.classList.remove('selected'));
+      this.classList.add('selected');
+
+      // Save it in Chrome
+      chrome.storage.sync.set({minSpeed: minSpeed});
+    });
+  });
+
+  mainButtons.forEach((button) => {
     button.addEventListener('click', function() {
       const speed = this.getAttribute('data-speed');
 
-      // Remode 'selected' class from the buttons but add it to just to clicked button
-      buttons.forEach((btn) => btn.classList.remove('selected'));
+      mainButtons.forEach((btn) => btn.classList.remove('selected'));
       this.classList.add('selected');
 
-      // Save it
       chrome.storage.sync.set({speed: speed});
     });
   });
+
+  rightButtons.forEach((button) => {
+    button.addEventListener('click', function() {
+      const maxSpeed = this.getAttribute('max-data-speed');
+
+      rightButtons.forEach((btn) => btn.classList.remove('selected'));
+      this.classList.add('selected');
+
+      chrome.storage.sync.set({maxSpeed: maxSpeed});
+    });
+  });
+
+
+
 });
 
