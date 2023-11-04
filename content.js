aec749b36ad26c2054df7813a648ca757e41ad52
dschil138 <davidschiller138@gmail.com>
2023-11-04 02:19:44 -0500

    originalSpeed finally working properly. Also fixed some persistentSpeed issues stemming fom stopPropagation(). All is working on youtube as well as embedded vids

--- content.js
+++ content.js
@@ -29,16 +29,13 @@ let lastPeriodKeyReleaseTime = 0;
 let lastCommaKeyReleaseTime = 0;
 let doubleTapAndHoldPeriod = false;
 let doubleTapAndHoldComma = false;
-const tier1 = 42;
-const tier2 = 140;
+const tier1 = 45;
+const tier2 = 165;
 const tier3 = 280;
-const verticalTier = 62;
-
-
+const verticalTier = 65;
 
 
 const overlayDiv = document.querySelector('.ytp-doubletap-ui-legacy');
-
 
 chrome.runtime.onMessage.addListener(
   function(request, sender, sendResponse) {
@@ -47,7 +44,6 @@ chrome.runtime.onMessage.addListener(
     }
   }
 );
-
 
 function syncSpeeds() {
   chrome.storage.sync.get('minSpeed', function(data) {
@@ -72,7 +68,6 @@ function syncSpeeds() {
   );
 }
 
-
 document.addEventListener("DOMContentLoaded", function() {
   let observer = new MutationObserver((mutations) => {
     mutations.forEach((mutation) => {
@@ -87,118 +82,32 @@ document.addEventListener("DOMContentLoaded", function() {
   });
   observer.observe(document.body, { childList: true, subtree: true });
 });
-
-
 indicator = document.createElement('div');
+
+
+
 
 function addIndicator(rate) {
   indicator.innerText = `${rate}x Speed${rate === 16 ? ' (max)' : ''}`;
   indicator.style.fontSize = '1.4em';
   indicator.style.fontWeight = 'normal';
-  indicator.style.backgroundColor = 'rgba(0, 0, 0, 0.35)';
+  indicator.style.backgroundColor = 'rgba(0, 0, 0, 0.45)';
   indicator.style.display = 'block';
 }
 
-
-function init(videoElement) {
-
-  syncSpeeds();
-  video = videoElement;
-
-  // remove the original overlay - we will be replacing it with our own overlay to avoid confusion
-  const overlay = document.querySelector('.ytp-speedmaster-overlay.ytp-overlay');
-  overlay?.remove();
-
-  video = document.querySelector('video');
-
-  if (lastVideoElement !== video && video !== null) {
-
-    indicator.classList.add('indicator');
-    video.parentElement.appendChild(indicator);
-
-    const moviePlayer = videoElement.closest('#movie_player');
-  
-    if (moviePlayer) {
-      moviePlayer.addEventListener('mousedown', mousedownHandler.bind(null, moviePlayer), true);
-
-      moviePlayer.addEventListener('mouseup', mouseupHandler.bind(null, moviePlayer), true);
-
-      moviePlayer.addEventListener('click', clickHandler.bind(null, moviePlayer), true);
-
-
-      moviePlayer.addEventListener('keydown', function (e) {
-        if (e.key === '.') {
-          let currentTime = Date.now();
-          let timeDifference = currentTime - lastPeriodKeyReleaseTime;
-      
-          if (timeDifference < 400) {
-            doubleTapAndHoldPeriod = true;
-          }
-
-          if (doubleTapAndHoldPeriod) {
-            isPeriodKeyDown = true;
-
-            // video.playbackRate = periodKeySpeed*2;
-            video.playbackRate = Math.min(periodKeySpeed * 2, 16);
-
-            addIndicator(Math.min(periodKeySpeed * 2, 16));
-          } else {
-            video.playbackRate = periodKeySpeed;
-            addIndicator(periodKeySpeed);
-          }
-
-        } else if (e.key === ',') {
-          let currentTime = Date.now();
-          let timeDifference = currentTime - lastCommaKeyReleaseTime;
-      
-          if (timeDifference < 400) {
-            doubleTapAndHoldComma = true;
-          }
-          if (doubleTapAndHoldComma) {
-            isCommaKeyDown = true;
-            video.playbackRate = 0.75;
-            addIndicator(0.75);
-          } else {
-            video.playbackRate = commaKeySpeed;
-            addIndicator(commaKeySpeed);
-          }
-        }
-      });
-      
-      moviePlayer.addEventListener('keyup', function (e) {
-        if (e.key === '.') {
-          doubleTapAndHoldPeriod = false;
-          lastPeriodKeyReleaseTime = Date.now();
-          isPeriodKeyDown = false;
-          indicator.style.display = 'none';
-          if (!isPeriodKeyDown) {
-            video.playbackRate = 1;
-          }
-
-        }
-        if (e.key === ',') {
-          doubleTapAndHoldComma = false;
-          lastCommaKeyReleaseTime = Date.now();
-          isCommaKeyDown = false;
-          indicator.style.display = 'none';
-          if (!isCommaKeyDown) {
-            video.playbackRate = 1;
-          }
-        }
-      });
-
-
-    }
-  }
-} // End init
-
-
-
+function findOriginalSpeed() {
+  return new Promise(resolve => {
+    const label = Array.from(document.querySelectorAll('.ytp-menuitem-label'))
+                      .find(el => el.textContent === 'Playback speed');
+    const content = label ? label.nextElementSibling.textContent.toLowerCase() : '1';
+    const originalSpeed = content === 'normal' ? '1' : content;
+    resolve(originalSpeed);
+  });
+}
 
 
 function simulateLeftArrowKeyPress() {
   video.focus();
-
   const leftArrowKeyCode = 37;
   const downEvent = new KeyboardEvent('keydown', {
       key: 'ArrowLeft',
@@ -222,7 +131,6 @@ video.dispatchEvent(upEvent);
 }
 
 
-
 function newSpeed(rate) {
   video.playbackRate = rate;
   indicator.innerText = `${rate}x Speed`;
@@ -232,28 +140,115 @@ function newSpeed(rate) {
 }
 
 
+function init(videoElement) {
+  syncSpeeds();
+  video = videoElement;
+
+  // remove the original 2x speed overlay - we will be replacing it with our own overlay to avoid confusion
+  const overlay = document.querySelector('.ytp-speedmaster-overlay.ytp-overlay');
+  overlay?.remove();
+  // remove pause overlay for embedded videos as it causes incosistent pausing behavior
+  const pauseOverlay = document.querySelector('.ytp-pause-overlay');
+  pauseOverlay?.remove();
+
+  video = document.querySelector('video');
+
+  if (lastVideoElement !== video && video !== null) {
+
+    indicator.classList.add('indicator');
+    video.parentElement.appendChild(indicator);
+    const moviePlayer = videoElement.closest('#movie_player');
+  
+    if (moviePlayer) {
+      moviePlayer.addEventListener('mousedown', mousedownHandler.bind(null, moviePlayer), true);
+      moviePlayer.addEventListener('mouseup', mouseupHandler.bind(null, moviePlayer), true);
+      moviePlayer.addEventListener('click', clickHandler.bind(null, moviePlayer), true);
+
+      moviePlayer.addEventListener('keydown', function (e) {
+        if (e.key === '.') {
+          let currentTime = Date.now();
+          let timeDifference = currentTime - lastPeriodKeyReleaseTime;
+      
+          if (timeDifference < 400) {
+            doubleTapAndHoldPeriod = true;
+          }
+
+          // double tap and hold period will give us double speed, up to max of 16x
+          if (doubleTapAndHoldPeriod) {
+            isPeriodKeyDown = true;
+            video.playbackRate = Math.min(periodKeySpeed * 2, 16);
+            addIndicator(Math.min(periodKeySpeed * 2, 16));
+          } else {
+            video.playbackRate = periodKeySpeed;
+            addIndicator(periodKeySpeed);
+          }
+
+        } else if (e.key === ',') {
+          let currentTime = Date.now();
+          let timeDifference = currentTime - lastCommaKeyReleaseTime;
+      
+          if (timeDifference < 400) {
+            doubleTapAndHoldComma = true;
+          }
+          // double tap and hold comma will give us 0.75x speed
+          if (doubleTapAndHoldComma) {
+            isCommaKeyDown = true;
+            video.playbackRate = 0.75;
+            addIndicator(0.75);
+          } else {
+            video.playbackRate = commaKeySpeed;
+            addIndicator(commaKeySpeed);
+          }
+        }
+      });
+      
+      moviePlayer.addEventListener('keyup', function (e) {
+        if (e.key === '.') {
+          doubleTapAndHoldPeriod = false;
+          lastPeriodKeyReleaseTime = Date.now();
+          isPeriodKeyDown = false;
+          indicator.style.display = 'none';
+          if (!isPeriodKeyDown) {
+            video.playbackRate = 1;
+          }
+        }
+        if (e.key === ',') {
+          doubleTapAndHoldComma = false;
+          lastCommaKeyReleaseTime = Date.now();
+          isCommaKeyDown = false;
+          indicator.style.display = 'none';
+          if (!isCommaKeyDown) {
+            video.playbackRate = 1;
+          }
+        }
+      });
+
+    }
+  }
+} // End init
 
 
 
+
+// MOUSE DOWN HANDLER
 function mousedownHandler(moviePlayer, e) {
-
   initialX = e.clientX;
   initialY = e.clientY;
   setPersistentSpeed = false;
     
-    longPressTimer = setTimeout(() => {
+    longPressTimer = setTimeout(async () => {
       if (!speedPersisting) {
-        originalSpeed = video.playbackRate;
+        originalSpeed = await findOriginalSpeed();
       }
       video.playbackRate = mainSpeed;
       addIndicator(mainSpeed);
-
       longPressFlag = true;
-
       moviePlayer.addEventListener('mousemove', handleMouseMove.bind(null, moviePlayer), true);
     }, 220);
 }
 
+
+// MOUSE UP HANDLER
 function mouseupHandler(moviePlayer, e) {
   moviePlayer.removeEventListener('mousemove', handleMouseMove, true);
   firstRewind = true;
@@ -267,37 +262,40 @@ function mouseupHandler(moviePlayer, e) {
   if (setPersistentSpeed) {
     setTimeout(() => {
       indicator.style.display = 'none';
-    }, 1500);
+    }, 1250);
   } else {
     indicator.style.display = 'none';
   }
-
-  if (longPressFlag) {
-    e.stopPropagation();
-    e.preventDefault();
-    if (speedPersisting && !setPersistentSpeed) {
-      video.playbackRate = originalSpeed;
-    } else if (setPersistentSpeed) {
-      video.playbackRate = newPersistentSpeed;
-      speedPersisting = true;
-    } else {
-      video.playbackRate = originalSpeed;
-    }
-  }
 }
 
+
+// CLICK HANDLER
 function clickHandler(moviePlayer, e) {
   clearInterval(rewindInterval);
   rewindInterval = null;
 
   if (longPressFlag) {
+
+    if (speedPersisting && !setPersistentSpeed) {
+      video.playbackRate = originalSpeed;
+      speedPersisting = false;
+
+    } else if (setPersistentSpeed) {
+      video.playbackRate = newPersistentSpeed;
+      speedPersisting = true;
+    } else {
+      video.playbackRate = originalSpeed;
+      speedPersisting = false;
+    }
+
+    longPressFlag = false;
     e.stopPropagation();
     e.preventDefault();
-    longPressFlag = false;
   }
 }
 
 
+// MOUSE MOVE HANDLER
 function handleMouseMove(moviePlayer, e) {
   if (!longPressFlag) return;
   const deltaX = e.clientX - initialX;
@@ -332,14 +330,12 @@ function handleMouseMove(moviePlayer, e) {
     setPersistentSpeed = true;
     newPersistentSpeed = video.playbackRate;
     indicator.style.fontWeight = 'bold';
-    indicator.style.backgroundColor = 'rgba(0, 0, 0, 0.45)';
-
+    indicator.style.backgroundColor = 'rgba(0, 0, 0, 0.6)';
   } else {
     setPersistentSpeed = false;
     indicator.style.fontWeight = 'normal';
-    indicator.style.backgroundColor = 'rgba(0, 0, 0, 0.35)';
+    indicator.style.backgroundColor = 'rgba(0, 0, 0, 0.45)';
   }
-
 }
 
 
@@ -350,4 +346,4 @@ setTimeout(() => {
   if (videoElement) {
     init(videoElement);
   }
-}, 800);
+}, 400);
